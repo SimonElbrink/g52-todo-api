b0a1509a16bf05847f6b5babd9e86bcbd7451b54
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-01-28 12:10:46 +0100

    TODO API - Confirming Role Service (End of Part 1) ✨

--- src/main/java/se/lexicon/g52todoapi/G52TodoApiApplication.java
+++ src/main/java/se/lexicon/g52todoapi/G52TodoApiApplication.java
@@ -1,13 +1,26 @@
 package se.lexicon.g52todoapi;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import se.lexicon.g52todoapi.service.RoleService;
 
 @SpringBootApplication
 public class G52TodoApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(G52TodoApiApplication.class, args);
+    }
+
+
+    @Bean
+    public CommandLineRunner runner (RoleService roleService){
+        return (args)-> {
+
+            roleService.getAll().forEach(System.out::println);
+
+        } ;
     }
 
 }

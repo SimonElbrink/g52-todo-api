aed1b691158597bebf439c7137afccd56aa4d949
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-01-28 10:09:40 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/g52todoapi/G52TodoApiApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g52todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G52TodoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G52TodoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g52todoapi/G52TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g52todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G52TodoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

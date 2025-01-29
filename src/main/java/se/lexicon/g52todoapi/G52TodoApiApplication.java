9344408824efe535f87e6063a06d9697f1aacdf2
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-01-29 10:15:56 +0100

    TODO API - Showing Debugger

--- src/main/java/se/lexicon/g52todoapi/G52TodoApiApplication.java
+++ src/main/java/se/lexicon/g52todoapi/G52TodoApiApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import se.lexicon.g52todoapi.domain.dto.RoleDTOView;
 import se.lexicon.g52todoapi.service.RoleService;
+
+import java.util.List;
 
 @SpringBootApplication
 public class G52TodoApiApplication {
@@ -18,7 +21,13 @@ public class G52TodoApiApplication {
     public CommandLineRunner runner (RoleService roleService){
         return (args)-> {
 
-            roleService.getAll().forEach(System.out::println);
+            List<RoleDTOView> all = roleService.getAll();
+//↖️Add breakpoint 🔴
+            // Add Breakpoint then run Debugger to see how the program executes your code.
+            // (Be aware of long routes. If you decide to go into Java/Spring Source code.
+            //  Remember to "Step out" if you get to deep.)
+
+            all.forEach(System.out::println);
 
         } ;
     }

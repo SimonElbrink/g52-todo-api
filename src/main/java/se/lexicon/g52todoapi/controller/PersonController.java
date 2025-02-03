9088e400953fa546f8db34d28a539be64cfd9a68
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-02-03 13:42:18 +0100

    TODO API - RestFul API - Added practice Person & Task Controller (Exercise)

---
+++ src/main/java/se/lexicon/g52todoapi/controller/PersonController.java
@@ -0,0 +1,5 @@
+package se.lexicon.g52todoapi.controller;
+
+public class PersonController {
+    //Todo: Implement Controller
+}
---
+++ src/main/java/se/lexicon/g52todoapi/controller/TaskController.java
@@ -0,0 +1,5 @@
+package se.lexicon.g52todoapi.controller;
+
+public class TaskController {
+    //Todo: Implement Controller
+}
